b8809cd48f4ae479b54b366265a99d974dd4e29a
TomStern <thomas@rightscale.com>
2013-01-29 21:19:50 +0000

    splunk fix 2

--- cookbooks/splunk/recipes/default.rb
+++ cookbooks/splunk/recipes/default.rb
@@ -1,4 +1,3 @@
-#
 # Cookbook Name:: splunk
 # Recipe:: default
 #
@@ -6,38 +5,32 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-# Cookbook Name:: splunk
-# Recipe:: default
-#
-# Copyright 2013, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
+
 log "Installing Splunk - version 1"
 
 user "splunk" do
  action :create
-  shell "/bin/bash"
-  end 
+ shell "/bin/bash"
+end 
 
-  directory "/opt/splunk" do 
-   owner "splunk"
-    mode "0755"
-     action :create 
-     end 
+directory "/opt/splunk" do 
+ owner "splunk"
+ mode "0755"
+ action :create 
+end 
 
-     remote_file "/opt/splunk.tar" do 
-      source "https://rs-training-assets.s3.amazonaws.com/splunk-4.3.4-136012-Linux-x86_64.tar"
-      action :create_if_missing 
-       end 
+remote_file "/opt/splunk.tar" do
+ source "https://rs-training-assets.s3.amazonaws.com/splunk-4.3.4-136012-Linux-x86_64.tar"
+ action :create_if_missing 
+end 
 
-       bash "install_splunk" do
-        cwd "/opt"
-         code <<-EOH
-           tar -xvf splunk.tar
-            chown -R splunk /opt/splunk
-            EOH
-            end 
+bash "install_splunk" do
+ cwd "/opt"
+ code <<-EOH
+ tar -xvf splunk.tar
+ chown -R splunk /opt/splunk
+ EOH
+end 
 
-            package "tree"
+package "tree"
 
