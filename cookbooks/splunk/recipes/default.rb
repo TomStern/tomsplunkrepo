ec59f4df3e4b71df3ce6deac5efae5d22f00f675
TomStern <thomas@rightscale.com>
2013-01-29 20:08:39 +0000

    fixed special chars in file

--- cookbooks/splunk/recipes/default.rb
+++ cookbooks/splunk/recipes/default.rb
@@ -20,15 +20,15 @@ user "splunk" do
   shell "/bin/bash"
   end 
 
-  directory "/opt/splunk" do   
+  directory "/opt/splunk" do 
    owner "splunk"
-    mode "0755"   
+    mode "0755"
      action :create 
      end 
 
-     remote_file "/opt/splunk.tar" do   
+     remote_file "/opt/splunk.tar" do 
       source "https://rs-training-assets.s3.amazonaws.com/splunk-4.3.4-136012-Linux-x86_64.tar"
-       action :create_if_missing 
+      action :create_if_missing 
        end 
 
        bash "install_splunk" do
