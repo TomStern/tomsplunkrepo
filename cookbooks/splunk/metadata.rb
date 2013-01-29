9be8179719d65ccd3e39df21969c41d90f3cc61e
TomStern <thomas@rightscale.com>
2013-01-29 23:39:30 +0000

    added attributes to splunk cookbook

--- cookbooks/splunk/metadata.rb
+++ cookbooks/splunk/metadata.rb
@@ -4,5 +4,7 @@ license          "All rights reserved"
 description      "Installs/Configures splunk"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+depends "rightscale"
 recipe "splunk::default", "Installs splunk form a tar gile, as well as the package 'tree'"
+recipe "splunk::install", "Installs splunk on a tar file w Attributes and Defs"
 
